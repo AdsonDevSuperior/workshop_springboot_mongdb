750b710c81e6ca21649afaa241387c06fc345702
Adson Albino <adsonplaygg@gmail.com>
2022-11-06 14:00:51 -0300

    Conectando ao MongoDB

--- src/main/java/com/dev/workshopmongo/domain/User.java
+++ src/main/java/com/dev/workshopmongo/domain/User.java
@@ -4,9 +4,14 @@ import java.io.Serializable;
 
 import javax.annotation.Generated;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
---
+++ src/main/java/com/dev/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.dev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.workshopmongo.domain.User;
+import com.dev.workshopmongo.repository.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+	}
+}
